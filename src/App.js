16f7f29df3d514a5cbe89bd69fb4417d0fabd7e8
Wongsaton Kampusan <wongsatonkampusan@gmail.com>
2024-07-10 19:51:41 -0700

    Init project with React and Material UI

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "@mui/material";
+
+function App() {
+  return (
+    <div>
+      <h1>My Portfolio</h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+}
+
+export default App;
