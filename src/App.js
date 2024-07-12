cd90423410deedab4a1683b2f226d77d64961d93
Wongsaton Kampusan <wongsatonkampusan@gmail.com>
2024-07-11 17:28:26 -0700

    Create a menu and profile avatar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "@mui/material";
+import Header from "./components/Header";
 
 function App() {
+  const theme = "primary";
   return (
     <div>
+      <Header theme={theme}></Header>
       <h1>My Portfolio</h1>
-      <Button variant="contained" color="primary">
+      <Button variant="contained" color={theme}>
         Hello World
       </Button>
     </div>
